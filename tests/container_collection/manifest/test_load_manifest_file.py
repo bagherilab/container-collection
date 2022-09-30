aa14208ecb58a5a0b9765c5f219922199c91d9d6
jessicasyu <jessicayu@u.northwestern.edu>
2022-09-30 11:52:40 -0400

    Add placeholder to load manifest file tests

---
+++ tests/container_collection/manifest/test_load_manifest_file.py
@@ -0,0 +1,13 @@
+import unittest
+
+from container_collection.manifest import load_manifest_file
+
+
+class TestLoadManifestFile(unittest.TestCase):
+    def test_load_manifest_file_local_file_exists(self):
+        # TODO: method should return loaded dataframe with columns KEY / EXTENSION / LOCATION
+        pass
+
+    def test_load_manifest_file_file_does_not_exist(self):
+        # TODO: method should return empty dataframe with columns KEY / EXTENSION / LOCATION
+        pass
