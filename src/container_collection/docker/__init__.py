998dadc31ca48663a25c6e5bceec660aaf56ac4a
jessicasyu <jesyu@uw.edu>
2022-10-24 21:12:30 -0400

    Add run docker command task

--- src/container_collection/docker/__init__.py
+++ src/container_collection/docker/__init__.py
@@ -2,5 +2,6 @@ from .check_docker_job import check_docker_job
 from .clean_docker_job import clean_docker_job
 from .get_docker_logs import get_docker_logs
 from .make_docker_job import make_docker_job
+from .run_docker_command import run_docker_command
 from .submit_docker_job import submit_docker_job
 from .terminate_docker_job import terminate_docker_job
