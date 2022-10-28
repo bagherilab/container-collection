f1e2394e861b2a597bd503d99be4a03878a39a7e
HebedaSupreme <mychal98@cs.washington.edu>
2022-10-28 15:17:02 -0700

    generate_input_contents unit tests

--- src/container_collection/template/generate_input_contents.py
+++ src/container_collection/template/generate_input_contents.py
@@ -3,7 +3,7 @@ from jinja2 import Template, StrictUndefined
 
 
 @task
-def generate_input_contents(template: str, conditions: dict) -> list[str]:
+def generate_input_contents(template: str, conditions: list[dict]) -> list[str]:
     compiled_template = Template(template, undefined=StrictUndefined)
     rendered_templates = [compiled_template.render(condition) for condition in conditions]
     return rendered_templates
