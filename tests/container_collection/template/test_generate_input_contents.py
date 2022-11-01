fc4898e58d698a4587324aa4bc0cb98aa59885f3
HebedaSupreme <mychal98@cs.washington.edu>
2022-11-01 09:47:55 -0700

    black test_generate_input_contents

--- tests/container_collection/template/test_generate_input_contents.py
+++ tests/container_collection/template/test_generate_input_contents.py
@@ -1,38 +1,71 @@
 import unittest
-from container_collection.template.generate_input_contents import generate_input_contents
+from container_collection.template.generate_input_contents import (
+    generate_input_contents,
+)
 from jinja2.exceptions import UndefinedError
 
 
 class TestGenerateInputContents(unittest.TestCase):
-
-    def test_generate_input_conditions_single_condition_template_single_condition_dict_success(self):
-        actual_rendered = generate_input_contents.fn('Lorem ipsum {{condition_a}}',
-                                                     [{'condition_a': 'This is condition A!'}])
-        expected = ['Lorem ipsum This is condition A!']
+    def test_generate_input_conditions_single_condition_template_single_condition_dict_success(
+        self,
+    ):
+        actual_rendered = generate_input_contents.fn(
+            "Lorem ipsum {{condition_a}}", [{"condition_a": "This is condition A!"}]
+        )
+        expected = ["Lorem ipsum This is condition A!"]
         self.assertCountEqual(expected, actual_rendered)
 
-    def test_generate_input_conditions_double_condition_template_double_condition_dict_success(self):
-        actual_rendered = generate_input_contents.fn('Lorem ipsum {{condition_a}} {{condition_b}}',
-                                                     [{'condition_a': 'This is condition A!',
-                                                       'condition_b': 'This is condition B!'}])
-        expected = ['Lorem ipsum This is condition A! This is condition B!']
+    def test_generate_input_conditions_double_condition_template_double_condition_dict_success(
+        self,
+    ):
+        actual_rendered = generate_input_contents.fn(
+            "Lorem ipsum {{condition_a}} {{condition_b}}",
+            [
+                {
+                    "condition_a": "This is condition A!",
+                    "condition_b": "This is condition B!",
+                }
+            ],
+        )
+        expected = ["Lorem ipsum This is condition A! This is condition B!"]
         self.assertCountEqual(expected, actual_rendered)
 
-    def test_generate_input_conditions_single_condition_template_two_single_condition_dicts_success(self):
-        actual_rendered = generate_input_contents.fn('Lorem ipsum {{condition_a}}',
-                                                     [{'condition_a': 'This is condition A!'},
-                                                      {'condition_a': 'This is condition B!'}])
-        expected = ['Lorem ipsum This is condition A!', 'Lorem ipsum This is condition B!']
+    def test_generate_input_conditions_single_condition_template_two_single_condition_dicts_success(
+        self,
+    ):
+        actual_rendered = generate_input_contents.fn(
+            "Lorem ipsum {{condition_a}}",
+            [
+                {"condition_a": "This is condition A!"},
+                {"condition_a": "This is condition B!"},
+            ],
+        )
+        expected = [
+            "Lorem ipsum This is condition A!",
+            "Lorem ipsum This is condition B!",
+        ]
         self.assertCountEqual(expected, actual_rendered)
 
-    def test_generate_input_conditions_single_condition_template_double_condition_dict_success(self):
-        actual_rendered = generate_input_contents.fn('Lorem ipsum {{condition_a}}',
-                                                     [{'condition_a': 'This is condition A!',
-                                                       'condition_b': 'This is condition B!'}])
-        expected = ['Lorem ipsum This is condition A!']
+    def test_generate_input_conditions_single_condition_template_double_condition_dict_success(
+        self,
+    ):
+        actual_rendered = generate_input_contents.fn(
+            "Lorem ipsum {{condition_a}}",
+            [
+                {
+                    "condition_a": "This is condition A!",
+                    "condition_b": "This is condition B!",
+                }
+            ],
+        )
+        expected = ["Lorem ipsum This is condition A!"]
         self.assertCountEqual(expected, actual_rendered)
 
-    def test_generate_input_conditions_double_condition_template_single_condition_dict_failure(self):
+    def test_generate_input_conditions_double_condition_template_single_condition_dict_failure(
+        self,
+    ):
         with self.assertRaises(UndefinedError):
-            generate_input_contents.fn('Lorem ipsum {{condition_a}} {{condition_b}}',
-                                       [{'condition_a': 'This is condition A!'}])
+            generate_input_contents.fn(
+                "Lorem ipsum {{condition_a}} {{condition_b}}",
+                [{"condition_a": "This is condition A!"}],
+            )
