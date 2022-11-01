43629b5b273e99af848cfa68ce13b3fd39159bfb
Jessica S. Yu <jesyu@uw.edu>
2022-10-31 20:31:02 -0400

    Add docker volumes tasks (#16)
    * Upgrade pytest version
    
    * Add docker volume tasks
    
    * Update submit docker job task to use volumes
    
    * Fix dependencies
    
    * Update run docker command task to use volumes

--- src/container_collection/docker/run_docker_command.py
+++ src/container_collection/docker/run_docker_command.py
@@ -1,9 +1,30 @@
+from typing import Optional
+
 import docker
 from prefect import task
+from prefect.blocks.system import Secret
 
 
 @task
-def run_docker_command(image: str, command: list[str]) -> None:
+def run_docker_command(
+    image: str,
+    command: list[str],
+    volume: Optional[docker.models.volumes.Volume],
+    credentials: bool = False,
+) -> None:
     client = docker.DockerClient(base_url="unix://var/run/docker.sock")
-    container = client.containers.run(image, command, detach=True)
-    container.wait()
+
+    if volume is None:
+        environment = []
+
+        if credentials:
+            aws_access_key_id = Secret.load("aws-access-key-id").get()
+            aws_secret_access_key = Secret.load("aws-secret-access-key").get()
+            environment.append(f"AWS_ACCESS_KEY_ID={aws_access_key_id}")
+            environment.append(f"AWS_SECRET_ACCESS_KEY={aws_secret_access_key}")
+
+        client.containers.run(image, command, environment=environment, auto_remove=True)
+    else:
+        client.containers.run(
+            image, command, volumes={volume.name: {"bind": "/mnt", "mode": "rw"}}, auto_remove=True
+        )
