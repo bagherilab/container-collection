fbb06a9efaf9dc76ed33b60915f95f95b2a47b39
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-03-28 16:58:27 -0400

    Update adding task decorator to init imports (#45)

--- src/container_collection/template/__init__.py
+++ src/container_collection/template/__init__.py
@@ -1,14 +1,15 @@
 import importlib
-import os
 import sys
 
 from prefect import task
 
-for module_file in os.listdir(os.path.dirname(__file__)):
-    if "__" in module_file or not module_file.endswith(".py"):
-        continue
+from .generate_input_contents import generate_input_contents
 
-    module_name = module_file.replace(".py", "")
+TASK_MODULES = [
+    generate_input_contents,
+]
 
-    module = importlib.import_module(f".{module_name}", package=__name__)
-    setattr(sys.modules[__name__], module_name, task(getattr(module, module_name)))
+for task_module in TASK_MODULES:
+    MODULE_NAME = task_module.__name__
+    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
+    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
