4f1e531b4cb7a62c93ee37d725e1a3806a2844b7
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-03-29 19:01:34 -0400

    Add task decorator to init imports

--- src/container_collection/manifest/__init__.py
+++ src/container_collection/manifest/__init__.py
@@ -1,6 +1,3 @@
-import importlib
-import sys
-
 from prefect import task
 
 from .filter_manifest_files import filter_manifest_files
@@ -8,14 +5,7 @@ from .find_missing_conditions import find_missing_conditions
 from .summarize_manifest_files import summarize_manifest_files
 from .update_manifest_contents import update_manifest_contents
 
-TASK_MODULES = [
-    filter_manifest_files,
-    find_missing_conditions,
-    summarize_manifest_files,
-    update_manifest_contents,
-]
-
-for task_module in TASK_MODULES:
-    MODULE_NAME = task_module.__name__
-    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
-    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
+filter_manifest_files = task(filter_manifest_files)
+find_missing_conditions = task(find_missing_conditions)
+summarize_manifest_files = task(summarize_manifest_files)
+update_manifest_contents = task(update_manifest_contents)
