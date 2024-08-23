008b037134748f2588140898960cb3477e296f94
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-08-23 18:16:20 -0400

    Combine include exclude tests into subtests

--- tests/container_collection/manifest/test_filter_manifest_files.py
+++ tests/container_collection/manifest/test_filter_manifest_files.py
@@ -1,6 +1,8 @@
 import unittest
-from container_collection.manifest.filter_manifest_files import filter_manifest_files
+
 import pandas as pd
+
+from container_collection.manifest.filter_manifest_files import filter_manifest_files
 
 
 class TestFilterManifestFiles(unittest.TestCase):
@@ -104,7 +106,7 @@ class TestFilterManifestFiles(unittest.TestCase):
                 },
             },
         }
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -167,7 +169,7 @@ class TestFilterManifestFiles(unittest.TestCase):
         include_filters = []
         exclude_filters = ["*"]
         expected_selection = {}
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -175,7 +177,7 @@ class TestFilterManifestFiles(unittest.TestCase):
         )
         self.assertEqual(expected_selection, actual_selection)
 
-    def test_filter_manifest_files_include_all_keys_key_with_incomplete_extensions(self):
+    def test_filter_manifest_files_key_with_incomplete_extensions(self):
         contents = [
             [
                 "name_key_A_0000",
@@ -239,7 +241,7 @@ class TestFilterManifestFiles(unittest.TestCase):
                 },
             },
         }
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -302,7 +304,7 @@ class TestFilterManifestFiles(unittest.TestCase):
         include_filters = ["*_0001"]
         exclude_filters = ["*_0001"]
         expected_selection = {}
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -388,7 +390,7 @@ class TestFilterManifestFiles(unittest.TestCase):
                 },
             },
         }
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -484,7 +486,7 @@ class TestFilterManifestFiles(unittest.TestCase):
                 },
             },
         }
-        actual_selection = filter_manifest_files.fn(
+        actual_selection = filter_manifest_files(
             manifest=manifest,
             extensions=self.extensions,
             include_filters=include_filters,
@@ -492,7 +494,7 @@ class TestFilterManifestFiles(unittest.TestCase):
         )
         self.assertEqual(expected_selection, actual_selection)
 
-    def test_filter_manifest_files_include_B_exclude_0000(self):
+    def test_filter_manifest_files_include_and_exclude_filters(self):
         contents = [
             [
                 "name_key_A_0000",
@@ -544,98 +546,48 @@ class TestFilterManifestFiles(unittest.TestCase):
             ],
         ]
         manifest = pd.DataFrame(contents, columns=self.manifest_columns)
-        include_filters = ["*B*"]
-        exclude_filters = ["*_0000"]
-        expected_selection = {
-            "name_key_B_0001": {
-                "extension_a": {
-                    "location": "/local/path/",
-                    "key": "full/file/name_key_B_0001.extension_a",
-                },
-                "extension_b": {
-                    "location": "/local/path/",
-                    "key": "full/file/name_key_B_0001.extension_b",
-                },
-            },
-        }
-        actual_selection = filter_manifest_files.fn(
-            manifest=manifest,
-            extensions=self.extensions,
-            include_filters=include_filters,
-            exclude_filters=exclude_filters,
-        )
-        self.assertEqual(expected_selection, actual_selection)
 
-    def test_filter_manifest_files_include_0000_exclude_B(self):
-        contents = [
-            [
-                "name_key_A_0000",
-                "extension_a",
-                "s3://bucket-name",
-                "full/file/name_key_A_0000.extension_a",
-            ],
-            [
-                "name_key_A_0000",
-                "extension_b",
-                "s3://bucket-name",
-                "full/file/name_key_A_0000.extension_b",
-            ],
-            [
-                "name_key_A_0001",
-                "extension_a",
-                "s3://bucket-name",
-                "full/file/name_key_A_0001.extension_a",
-            ],
-            [
-                "name_key_A_0001",
-                "extension_b",
-                "s3://bucket-name",
-                "full/file/name_key_A_0001.extension_b",
-            ],
-            [
-                "name_key_B_0000",
-                "extension_a",
-                "/local/path/",
-                "full/file/name_key_B_0000.extension_a",
-            ],
-            [
-                "name_key_B_0000",
-                "extension_b",
-                "/local/path/",
-                "full/file/name_key_B_0000.extension_b",
-            ],
-            [
-                "name_key_B_0001",
-                "extension_a",
-                "/local/path/",
-                "full/file/name_key_B_0001.extension_a",
-            ],
-            [
-                "name_key_B_0001",
-                "extension_b",
-                "/local/path/",
-                "full/file/name_key_B_0001.extension_b",
-            ],
+        parameters = [
+            (
+                ["*B*"],
+                ["*_0000"],
+                {
+                    "name_key_B_0001": {
+                        "extension_a": {
+                            "location": "/local/path/",
+                            "key": "full/file/name_key_B_0001.extension_a",
+                        },
+                        "extension_b": {
+                            "location": "/local/path/",
+                            "key": "full/file/name_key_B_0001.extension_b",
+                        },
+                    },
+                },
+            ),
+            (
+                ["*_0000"],
+                ["*B*"],
+                {
+                    "name_key_A_0000": {
+                        "extension_a": {
+                            "location": "s3://bucket-name",
+                            "key": "full/file/name_key_A_0000.extension_a",
+                        },
+                        "extension_b": {
+                            "location": "s3://bucket-name",
+                            "key": "full/file/name_key_A_0000.extension_b",
+                        },
+                    },
+                },
+            ),
         ]
-        manifest = pd.DataFrame(contents, columns=self.manifest_columns)
-        include_filters = ["*_0000"]
-        exclude_filters = ["*B*"]
-        expected_selection = {
-            "name_key_A_0000": {
-                "extension_a": {
-                    "location": "s3://bucket-name",
-                    "key": "full/file/name_key_A_0000.extension_a",
-                },
-                "extension_b": {
-                    "location": "s3://bucket-name",
-                    "key": "full/file/name_key_A_0000.extension_b",
-                },
-            },
-        }
-        actual_selection = filter_manifest_files.fn(
-            manifest=manifest,
-            extensions=self.extensions,
-            include_filters=include_filters,
-            exclude_filters=exclude_filters,
-        )
-        self.assertEqual(expected_selection, actual_selection)
+
+        for include_filters, exclude_filters, expected_selection in parameters:
+            with self.subTest(include=include_filters, exclude=exclude_filters):
+                actual_selection = filter_manifest_files(
+                    manifest=manifest,
+                    extensions=self.extensions,
+                    include_filters=include_filters,
+                    exclude_filters=exclude_filters,
+                )
+                self.assertEqual(expected_selection, actual_selection)
