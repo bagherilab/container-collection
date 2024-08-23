6244de881d94d9883259dec8f6d7664ee632a452
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-08-23 16:33:19 -0400

    Add docstring for generate input contents task

--- src/container_collection/template/generate_input_contents.py
+++ src/container_collection/template/generate_input_contents.py
@@ -2,6 +2,22 @@ from jinja2 import StrictUndefined, Template
 
 
 def generate_input_contents(template: str, conditions: list[dict]) -> list[str]:
+    """
+    Generate input contents from template and conditions.
+
+    Parameters
+    ----------
+    template
+        Template string.
+    conditions
+        List of conditions.
+
+    Returns
+    -------
+    :
+        List of rendered templates.
+    """
+
     compiled_template = Template(template, undefined=StrictUndefined)
     rendered_templates = [compiled_template.render(condition) for condition in conditions]
     return rendered_templates
