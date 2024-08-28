55c866ca32fa2afe91f9cc7fb3c59aeb87e051f6
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-08-28 10:34:25 -0400

    Add docstrings and unit tests for terminate batch job task

--- src/container_collection/batch/terminate_batch_job.py
+++ src/container_collection/batch/terminate_batch_job.py
@@ -2,8 +2,21 @@ from time import sleep
 
 import boto3
 
+TERMINATION_REASON = "Prefect workflow termination"
+
 
 def terminate_batch_job(job_arn: str) -> None:
+    """
+    Terminate job on AWS Batch.
+
+    Task will sleep for 1 minute after sending the termination request.
+
+    Parameters
+    ----------
+    job_arn
+        Job ARN.
+    """
+
     client = boto3.client("batch")
-    client.terminate_job(jobId=job_arn, reason="Prefect workflow termination")
+    client.terminate_job(jobId=job_arn, reason=TERMINATION_REASON)
     sleep(60)
