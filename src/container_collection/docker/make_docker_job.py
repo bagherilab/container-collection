bf168ab7c2078b0bf613d21c0959164f549d8b57
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-08-30 11:56:40 -0400

    Add docstrings and unit tests for make docker job task

--- src/container_collection/docker/make_docker_job.py
+++ src/container_collection/docker/make_docker_job.py
@@ -1,11 +1,42 @@
-def make_docker_job(name: str, image: str, index: int) -> dict:
-    return {
+from typing import Optional
+
+
+def make_docker_job(name: str, image: str, environment: Optional[list[str]] = None) -> dict:
+    """
+    Create docker job definition.
+
+    Environment variables are passed as strings using the following structure:
+
+    .. code-block:: python
+
+        [
+            "envName1=envValue1",
+            "envName2=envValue2",
+            ...
+        ]
+
+    Parameters
+    ----------
+    name
+        Job definition name.
+    image
+        Docker image.
+    environment
+        List of environment variables as strings.
+
+    Returns
+    -------
+    :
+        Job definition.
+    """
+
+    job_definition = {
         "image": image,
-        "name": f"{name}_{index}",
-        "environment": [
-            "SIMULATION_TYPE=LOCAL",
-            f"FILE_SET_NAME={name}",
-            f"JOB_ARRAY_INDEX={index}",
-        ],
+        "name": name,
         "volumes": ["/mnt"],
     }
+
+    if environment is not None:
+        job_definition["environment"] = environment
+
+    return job_definition
